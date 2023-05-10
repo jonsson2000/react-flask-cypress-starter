d497f6113f3542a75b2d33b7016a55e6f3789bff
Andreas Jonsson <andreas@andreasjonsson.de>
2023-05-10 21:15:10 +0200

    Debug env vars

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -17,11 +17,11 @@ describe("example to-do app", () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit(
-      Cypress.env("BASE_URL") |
+    console.log(
+      Cypress.env("BASE_URL") +
         ("?shipyard_token=" + Cypress.env("BYPASS_TOKEN"))
     );
-    // cy.visit("https://example.cypress.io/todo");
+    cy.visit("https://example.cypress.io/todo");
   });
 
   it("displays two todo items by default", () => {
