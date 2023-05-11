0a8884ce64d41bf47150d8d4985e61882bf20dd7
Andreas Jonsson <andreas@andreasjonsson.de>
2023-05-11 17:52:56 +0200

    Update test to assert that counter is more than 1000

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -12,5 +12,37 @@ describe("shipyard flask react example app", () => {
     cy.get(".makeStyles-img-4")
       .first()
       .should("have.attr", "alt", "Shipyard logo");
+
+    cy.get("button")
+      .contains("Reset Counter")
+      .click();
+
+    cy.reload(true);
+
+    cy.get("p")
+      .contains("Flask server running on port")
+      .should("contain", "1 time");
+  });
+
+  it("Counter should be larger than 1000", () => {
+    // TBD outline
+    // - Run the test. It succeeds
+    //
+    // - Visits the page manually, click reset counter
+    // - Go to GitHub Pull Request action
+    // - Re-run the test, it fails
+    // - Go to data dashboard
+    // - Roll back the database to a known good state
+    // - Re-run the test, it should succeed
+
+    cy.get("p")
+      .contains("Flask server running on port")
+      .then($countInfo => {
+        const count = parseInt(
+          $countInfo.text().match(/Pinged (\d+) time/)?.[1],
+          10
+        );
+        expect(count).to.be.greaterThan(1000);
+      });
   });
 });
